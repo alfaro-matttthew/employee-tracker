5d9d67322d170aca51ccb52506319df4ba3a4561
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-08-03 15:51:58 -0400

    wrote the placeholder for the seed SQL script

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+VALUES ("Web Development"),
+       ("Data Science"),
+       ("Math"),
+       ("Electives");
+
+INSERT INTO role (title, salary)
+VALUES ("Intro to JavaScript", 1),
+       ("Data Science", 2),
+       ("Linear Algebra", 3),
+       ("History of the Internet", 4),
+       ("Machine Learning", 4),
+       ("Game Design", 1 ),
+       ("Cloud Development", 1);
+
+       INSERT INTO employee (first_name, last_name)
+VALUES ("Intro to JavaScript", 1),
+       ("Data Science", 2),
+       ("Linear Algebra", 3),
+       ("History of the Internet", 4),
+       ("Machine Learning", 4),
+       ("Game Design", 1 ),
+       ("Cloud Development", 1);
