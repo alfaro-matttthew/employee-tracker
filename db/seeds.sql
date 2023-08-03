8ae0f09fe51fa9a6030044ae1f4cc330a1edd6a3
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-08-03 16:00:22 -0400

    completed the department values, tested and it worked, and then filled out the role values.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,20 @@
 INSERT INTO department (name)
-VALUES ("Web Development"),
-       ("Data Science"),
-       ("Math"),
-       ("Electives");
+VALUES ("Engineering"),
+       ("Executive"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales");
 
 INSERT INTO role (title, salary)
-VALUES ("Intro to JavaScript", 1),
-       ("Data Science", 2),
-       ("Linear Algebra", 3),
-       ("History of the Internet", 4),
-       ("Machine Learning", 4),
-       ("Game Design", 1 ),
-       ("Cloud Development", 1);
+VALUES ("Software Engineer", 45000),
+       ("Lead Engineer", 60000),
+       ("CEO", 450000),
+       ("Accountant", 125000),
+       ("Acount Manager", 160000),
+       ("Lawyer", 198000),
+       ("Legal Team Lead", 250000),
+       ("Salesperson", 80000),
+       ("Sales Lead", 100000);
 
        INSERT INTO employee (first_name, last_name)
 VALUES ("Intro to JavaScript", 1),
