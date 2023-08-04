469dd3e8d44053716e2aa224c3dc1b0495d30f89
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-08-03 23:44:46 -0400

    Connected to the database and connected some inquirer.

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 const express = require("express");
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const db = require("./db/connections");
+const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-  console.log(`Connected to the movies_db database.`)
-);
+db.connect((err) => {
+  if (err) {
+    console.error("error connecting to the database:", err.message);
+    return;
+  }
+  console.log("Connected to the datebase!");
 
-app.use((req, res) => {
-  res.status(404).end();
+  //routes go here
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+menu = [
+  {
+    type: "list",
+    name: "mainScreen",
+    message: "What would you like to do?",
+    choices: [
+      "View all Employees",
+      "View all Departments",
+      "View all Roles",
+      "View all Employees",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update an Employee Role",
+    ],
+  },
+];
+
+inquirer.prompt(menu).then((answers) => {
+  console.log("User selected:", answers.mainScreen);
+  let response = answers.mainScreen;
+  if (response == "View all Employees") {
+    console.log("View all Employees");
+  }
+  else if (response == "View all Departments") {
+    console.log("View all Departments");
+  }
+  else if (response == "View all Roles") {
+    console.log("View all Roles");
+  }
+  else if (response == "View all Employees") {
+    console.log("View all Employees");
+  }
+  else if (response == "Add a Department") {
+    console.log("Add a Department");
+  }
+  else if (response == "Add a Role") {
+    console.log("Add a Role");
+  }
+  else if (response == "Add an Employee") {
+    console.log("Add an Employee");
+  }
+  else {
+    console.log("Update an Employee Role");
+  }
 });
